5deb43b1799cb54fb93bbd0219e1c6a8c79e2955
Harsh Patel <48337893+harshpatel010202@users.noreply.github.com>
2024-06-01 13:43:46 -0700

    fix: fixed tables

--- backend/database/tables/encryption_messages.sql
+++ backend/database/tables/encryption_messages.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS Encryption_Messages (
     encryption_key_id SERIAL PRIMARY KEY,
-    chat_id INT NOT NULL REFERENCES Messages(chat_id) ON DELETE CASCADE,
+    chat_id INT NOT NULL REFERENCES Messages(chat_id) ON DELETE CASCADE
 );
--- backend/database/tables/messages.sql
+++ backend/database/tables/messages.sql
@@ -7,3 +7,5 @@ CREATE TABLE IF NOT EXISTS Messages (
     timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     chat_id INT NOT NULL
 );
+
+ALTER TABLE Messages ADD CONSTRAINT chat_id_unique UNIQUE (chat_id);
