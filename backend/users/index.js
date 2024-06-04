e03fb28c28648bff90384009701fe79fb5e3764f
Rena Kollmann-Suhr <renakollmannsuhr@uvic.ca>
2024-06-04 11:44:57 -0700

    changed filenames

--- backend/users/index.js
+++ backend/users/index.js
@@ -1,6 +1,6 @@
 import express from "express";
-import userdata from "./routes/userdata.js";
-import searchhistory from "./routes/searchhistory.js";
+import userdata from "./routes/user_data.js";
+import searchhistory from "./routes/search_history.js";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
