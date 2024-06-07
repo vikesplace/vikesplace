c0f53234173133696bf274f3c8e647a276d1e700
Amanda Anderson <aganderson@shaw.ca>
2024-06-06 19:30:37 -0700

    Create listings card component and display 4 sample listings

--- frontend/src/pages/ViewListings.js
+++ frontend/src/pages/ViewListings.js
@@ -2,14 +2,34 @@ import React from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import '../App.css';
+import ListingCard from '../components/ListingCard.js';
+
+// TODO: GET listings
+
+const listings = [
+  { id: '4', title: 'Test 1', price: '2.00', location: 'V9VW9W', status: 'AVAILABLE' },
+  { id: '10',title: 'Super cool object', price: '3.45', location: 'V9VW9W', status: 'SOLD' },
+  { id: '100',title: 'Buy Me!', price: '1234.56', location: 'V9VW9W', status: 'AVAILABLE' },
+  { id: '3',title: 'Another listings for sale', price: '98765432.10', location: 'V9VW9W', status: 'AVAILABLE' }
+];
 
 function ViewListings() {
   return (
     <div className="ViewListings">
       <Container>
         <Box mt={2}>
-            ViewListings
-          {/* Add your main content here */}
+          {listings.map((listing) => (
+            <div key={'div'+listing.id}>
+              <ListingCard
+                id={listing.id}
+                title={listing.title}
+                price={listing.price}
+                location={listing.location}
+                status={listing.status}          
+              />
+              <br />
+            </div>
+            ))}
         </Box>
       </Container>
     </div>
