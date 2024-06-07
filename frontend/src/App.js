1ace3eaf224fe47980d0c9a7e9faa904f5220f97
Amanda Anderson <aganderson@shaw.ca>
2024-06-07 12:18:53 -0700

    Added intial versions of pages to finish creating account and message upon account creation succss

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
 import NavBar from './NavBar';
+import Home from './pages/Home.js';
 import ViewListings from './pages/ViewListings.js';
 import CreateListing from './pages/CreateListing.js';
 import ManageListings from './pages/ManageListings.js';
 import Messages from './pages/Messages.js';
+import VerifyAccount from './pages/VerifyAccount.js';
+import VerifiedAccount from './pages/VerifiedAccount.js';
 
-function App() {
+function App() {  
   return (
     <Router>
       <NavBar />
       <Routes>
+        <Route path="/" element={<Home />} /> 
         <Route path="/view-listings" element={<ViewListings />} />
         <Route path="/create-listing" element={<CreateListing />} />
         <Route path="/manage-listings" element={<ManageListings />} />
         <Route path="/messages" element={<Messages />} />
-        {/* Add other routes here */}
+        <Route path="/verify" element={<VerifyAccount />} />
+        <Route path="/verified" element={<VerifiedAccount />} />
       </Routes>
     </Router>
   );
---
+++ frontend/src/pages/VerifyAccount.js
@@ -0,0 +1,116 @@
+import React, { useState } from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import '../App.css';
+import Button from '@mui/material/Button';
+import Grid from '@mui/material/Grid';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+
+
+function VerifyAccount() {
+    const [username, setUsername] = useState("");
+    const [usernameError, setUsernameError] = useState(false);
+    // TODO validate password/confirm password
+    // TODO validate address (is a real place?)
+    
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+        // TODO criteria for valid username?
+        // possible to return to this page after form submits and display errors?
+        if (event.target.value.length > 20) {
+            setUsernameError("Username is too long");
+        } else {
+            setUsernameError(false);
+        }
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        // TODO POST 
+        console.log({
+            username: data.get('username'),
+            password: data.get('password'),
+        });
+    }
+  return (
+    <div className="VerifyAccount">
+      <Container>
+      <Box
+          sx={{
+            marginTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+            <Typography component="h1" variant="h5">
+                Finish Creating Account
+            </Typography>
+            <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <TextField
+                        required
+                        fullWidth
+                        id="username"
+                        label="Username"
+                        name="username"
+                        autoComplete="username"
+                        value={username}
+                        onChange={handleUsernameChange}
+                        error={usernameError}
+                        helperText={usernameError}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="new-password"
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        required
+                        fullWidth
+                        name="confirm-password"
+                        label="Confirm Password"
+                        type="password"
+                        id="confirm-password"
+                        autoComplete="new-password"
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <TextField
+                        required
+                        fullWidth
+                        name="address"
+                        label="Address"
+                        type="address"
+                        id="address"
+                        autoComplete="street-address"
+                    />
+                </Grid>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                >
+                Sign Up
+                </Button>
+                </Grid>
+            </Box>
+        </Box>
+      </Container>
+    </div>
+  );
+}
+
+export default VerifyAccount;
