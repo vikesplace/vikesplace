960c19451166d6cb1c4d60d752a4c96c33d260cf
Harsh Patel <48337893+harshpatel010202@users.noreply.github.com>
2024-06-08 08:36:57 -0700

    be-id-119: setup the recommendation structure and fixed the packages (#15)
    * fix: fixed tables structure by adding constraints
    
    * feat: setup listing structure
    
    * setup database connection for listing
    
    * feat: setup listing structure
    
    * feat: updated README.md
    
    * fix: fixed the listing structure and removed the data layer code
    
    * fix: removed README.md
    
    * feat: setup the structure for search
    
    * fix: updated the structure of search
    
    * feat: setup the recommendation structure and fixed the packages
    
    * fix: added the microservices in the docker-compose.yml
    
    * fix: fixed tables formatting
    
    * fix: fixed tables formatting

--- backend/database/tables/reviews.sql
+++ backend/database/tables/reviews.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS Reviews (
     reviewed_listing_id INT NOT NULL REFERENCES Listings(listing_id) ON DELETE CASCADE,
     review_user_id INT NOT NULL REFERENCES Users(user_id) ON DELETE CASCADE,
     review_content TEXT NOT NULL,
+    listing_rating_id INT NOT NULL REFERENCES Ratings(listing_rating_id) ON DELETE CASCADE,
     timestamp TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
