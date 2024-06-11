7370ce36b816a4e4c9033af3ca4b4af1e16ddaa1
Harsh Patel <48337893+harshpatel010202@users.noreply.github.com>
2024-06-11 11:51:13 -0700

    feat: setup data layer for users

--- backend/data_layer/index.js
+++ backend/data_layer/index.js
@@ -3,6 +3,7 @@ import 'dotenv/config'
 import listingRoutes from "./routes/listing_routes.js";
 import ratingRoutes from "./routes/rating_routes.js";
 import reviewRoutes from "./routes/review_routes.js";
+import userRoutes from "./routes/user_routes.js";
 import db from "./config/database.js";
 
 const PORT = process.env.PORT || 5000;
@@ -14,6 +15,7 @@ app.use(express.json());
 app.use("/listing",listingRoutes);
 app.use("/rating",ratingRoutes);
 app.use("/review",reviewRoutes);
+app.use("/user",userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
