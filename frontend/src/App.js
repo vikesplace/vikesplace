eca558c15ecdfeff33a53e54e4a166fe7894c968
Amanda Anderson <aganderson@shaw.ca>
2024-06-11 11:45:18 -0700

    Move NavBar to components folder to be consistent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import NavBar from './NavBar';
+import NavBar from './components/NavBar.js';
 import ViewListings from './pages/ViewListings.js';
 import CreateListing from './pages/CreateListing.js';
 import ManageListings from './pages/ManageListings.js';
@@ -16,7 +16,7 @@ function App() {
         <Route path="/create-listing" element={<CreateListing />} />
         <Route path="/manage-listings" element={<ManageListings />} />
         <Route path="/messages" element={<Messages />} />
-        <Route path="/messages" element={<SearchHistory />} />
+        <Route path="/history" element={<SearchHistory />} />
         {/* Add other routes here */}
       </Routes>
     </Router>
