3257a7ce7220adeebfd26a3666bdc92d68e346dc
Amanda Anderson <75967325+AmandaGAnderson@users.noreply.github.com>
2024-06-11 17:19:17 -0700

    fe-id-94: View Listings, Added Search Bar, and Setup Testing (#24)
    * Create listings card component and display 4 sample listings
    
    * Resolving error images from creating the application
    
    * Create placeholder search bar
    
    * Improving search bar and adding history
    
    * Move NavBar to components folder to be consistent
    
    * Add search bar to home page
    
    * Add testing structure, remove NavBar from previous location

--- frontend/src/pages/SearchHistory.js
+++ frontend/src/pages/SearchHistory.js
@@ -3,13 +3,12 @@ import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import '../App.css';
 
-function Home() {
+function SearchHistory() {
   return (
-    <div className="Home">
+    <div className="SearchHistory">
       <Container>
-        <SearchBar />
         <Box mt={2}>
-            Home
+            SearchHistory
           {/* Add your main content here */}
         </Box>
       </Container>
@@ -17,4 +16,4 @@ function Home() {
   );
 }
 
-export default Home;
+export default SearchHistory;
