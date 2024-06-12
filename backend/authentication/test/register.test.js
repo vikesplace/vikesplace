0bc5412ce5ed7a4838d54845438204531dd47f1c
Aman Palod <palod.aman@gmail.com>
2024-06-11 18:20:19 -0700

    comment

--- backend/authentication/test/register.test.js
+++ backend/authentication/test/register.test.js
@@ -6,7 +6,7 @@ const identification = (req, res, next) => {
   console.log("Auth middleware logic here");
   next();
 };
-
+// dsfds
 const app = express();
 app.use(express.json());
 app.use(identification);
