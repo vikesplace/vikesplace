fa4de997ef0caeca68e84b2ee2906b7057221175
Harsh Patel <48337893+harshpatel010202@users.noreply.github.com>
2024-06-13 17:40:36 -0700

    be-id-194: Setup Data Layer for Search and Recommendation and Write Fail Test Cases for Listing (#34)
    * feat: setup data layer for ratings and reviews
    
    * feat: setup data layer for users
    
    * fix: updated the location data type from POINT to STRING
    
    * fix: removed console logs
    
    * fix: changed the location data type to POINT
    
    * fix: consistent naming
    
    * fix: plural naming
    
    * feat: setup recommendation and search
    
    * feat: added routes for search and recommendation
    
    * feat: added test cases that are suppose to fail
    
    * fix: fixed the spacing

--- backend/data_layer/index.js
+++ backend/data_layer/index.js
@@ -4,6 +4,11 @@ import listingRoutes from "./routes/listing_routes.js";
 import ratingRoutes from "./routes/rating_routes.js";
 import reviewRoutes from "./routes/review_routes.js";
 import userRoutes from "./routes/user_routes.js";
+import ratingRoutes from "./routes/rating_routes.js";
+import reviewRoutes from "./routes/review_routes.js";
+import userRoutes from "./routes/user_routes.js";
+import searchRoutes from "./routes/search_routes.js";
+import recommendationRoutes from "./routes/recommendation_routes.js";
 import db from "./config/database.js";
 
 const PORT = process.env.PORT || 5000;
@@ -16,6 +21,11 @@ app.use("/listing",listingRoutes);
 app.use("/rating",ratingRoutes);
 app.use("/review",reviewRoutes);
 app.use("/user",userRoutes);
+app.use("/rating",ratingRoutes);
+app.use("/review",reviewRoutes);
+app.use("/user",userRoutes);
+app.use("/search",searchRoutes);
+app.use("/recommendation",recommendationRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
---
+++ backend/data_layer/models/recommendation_models.js
@@ -0,0 +1,44 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/database.js";
+
+class Recommendations extends Model {}
+
+Recommendations.init(
+  {
+    recommendation_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    recommending_user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "users",
+        key: "user_id",
+      },
+      onDelete: "CASCADE",
+    },
+    recommended_listing_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "listings",
+        key: "listing_id",
+      },
+      onDelete: "CASCADE",
+    },
+    ignored: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false
+  }
+);
+
+export default Recommendations;
+  
