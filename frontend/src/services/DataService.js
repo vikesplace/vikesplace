f61522490738d11fabb3f2826b4759452c5d4816
Amanda Anderson <aganderson@shaw.ca>
2024-06-15 12:00:12 -0700

    Add remaining api's to DataService

--- frontend/src/services/DataService.js
+++ frontend/src/services/DataService.js
@@ -56,6 +56,78 @@ class DataService {
         });
     }
 
+    getUserSearchHistory(id) {
+        return axios.get(API_URL + 'users/' + {id} + '/searches', { 
+            headers: authHeader()
+        });
+    }
+
+    getReviews(id) {
+        return axios.get(API_URL + 'review/' + {id}, { 
+            headers: authHeader()
+        });
+    }
+
+    getRatings(id) {
+        return axios.get(API_URL + 'rating/' + {id}, { 
+            headers: authHeader()
+        });
+    }
+
+    createReview(id, reviewContent) {
+        return axios.post(API_URL + 'review/' + {id}, { 
+            headers: authHeader(),
+            reviewContent
+        });
+    }
+
+    createRating(id, ratingValue) {
+        return axios.post(API_URL + 'rating/' + {id}, { 
+            headers: authHeader(),
+            ratingValue
+        });
+    }
+
+    getRecommendations() {
+        return axios.get(API_URL + 'recommendations', { 
+            headers: authHeader()
+        });
+    }
+
+    ignoreRecommendation(id) {
+        return axios.post(API_URL + 'recommendations/' + {id} + '/ignore', { 
+            headers: authHeader(),
+            ignore: true
+        });
+    }
+
+    getChats(id) {
+        return axios.get(API_URL + 'chats', { 
+            headers: authHeader(),
+            id
+        });
+    }
+
+    getChatMessages(id) {
+        return axios.get(API_URL + 'messages/' + {id}, { 
+            headers: authHeader(),
+            id
+        });
+    }
+
+    getChatInformation(id) {
+        return axios.get(API_URL + 'chats/' + {id}, { 
+            headers: authHeader(),
+            id
+        });
+    }
+
+    updateMessages() {
+        //TODO modify to websocket
+        return axios.post(API_URL + 'messages/updates', { 
+            headers: authHeader()
+        });
+    }
 }
 
 export default new DataService();
