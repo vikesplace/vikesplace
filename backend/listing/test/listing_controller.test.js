8ac47771b14256989166e08802d82536d30126ad
Naoya Pimlott <naoyapimlott@Naoyas-MacBook-Air.local>
2024-06-18 01:32:21 -0700

    added tests for get seller listings endpoint

--- backend/listing/test/listing_controller.test.js
+++ backend/listing/test/listing_controller.test.js
@@ -1,17 +1,18 @@
 import axios from "axios";
 import { createListing } from "../controller/create_listing";
+import { getSellerListings } from "../controller/get_seller_listings";
 jest.mock("axios");
 
 describe("Listing Routes", () => {
   it("should create a listing", async () => {
-    axios.post.mockImplementation(() => Promise.resolve({ data: {message: "Create Listing"} }));
+    axios.post.mockImplementationOnce(() => Promise.resolve({ data: 1 }));
     let responseObject = {};
     const mockRes = {
-      body:{},
-      json: jest.fn().mockImplementation((result)=>{
+      body: {},
+      json: jest.fn().mockImplementationOnce((result) => {
         responseObject = result;
       }),
-      status: jest.fn()
+      status: jest.fn(),
     };
     await createListing(
       {
@@ -25,18 +26,20 @@ describe("Listing Routes", () => {
       },
       mockRes
     );
-    expect(responseObject).toEqual({ message: "Create Listing" });
+    expect(responseObject).toEqual(1);
   });
 
   it("it should fail to create", async () => {
-    axios.post.mockImplementation(() => Promise.resolve({ data: {message: "Unable to create listing"} }));
+    axios.post.mockImplementationOnce(() =>
+      Promise.resolve({ data: { message: "Invalid input data" } })
+    );
     let responseObject = {};
     const mockRes = {
-      body:{},
-      json: jest.fn().mockImplementation((result)=>{
+      body: {},
+      json: jest.fn().mockImplementationOnce((result) => {
         responseObject = result;
       }),
-      status: jest.fn()
+      status: jest.fn(),
     };
     await createListing(
       {
@@ -50,6 +53,58 @@ describe("Listing Routes", () => {
       },
       mockRes
     );
-    expect(responseObject).toEqual({ message: "Unable to create listing" });
+    expect(responseObject).toEqual({ message: "Invalid input data" });
+  });
+
+  it("should return all listingIds", async () => {
+    const mockOutput = [
+      {
+        listing_id: 1,
+      },
+      {
+        listing_id: 2,
+      },
+      {
+        listing_id: 3,
+      },
+    ];
+    const mockControllerOutput = [1, 2, 3];
+    axios.post.mockImplementationOnce(() =>
+      Promise.resolve({ data: mockOutput, status: 200 })
+    );
+    let responseObject = [];
+    const mockRes = {
+      body: {},
+      json: jest.fn().mockImplementationOnce((result) => {
+        console.log(result);
+        responseObject = result;
+      }),
+      status: jest.fn(),
+    };
+    await getSellerListings({}, mockRes);
+    expect(responseObject).toEqual(mockControllerOutput);
+  });
+
+  it("fail to return all listingIds", async () => {
+    axios.post.mockImplementationOnce(() =>
+      Promise.resolve({ data: { message: "Seller not found" } })
+    );
+    let responseObject = {};
+    const mockRes = {
+      body: {},
+      json: jest.fn().mockImplementationOnce((result) => {
+        responseObject = result;
+      }),
+      status: jest.fn(),
+    };
+    await getSellerListings(
+      {
+        body: {
+          seller_id: "1",
+        },
+      },
+      mockRes
+    );
+    expect(responseObject).toEqual({ message: "Seller not found" });
   });
 });
