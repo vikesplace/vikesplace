1d8a9487beedff08e9f450391abf1b99ef5c2966
Rena Kollmann-Suhr <renakollmannsuhr@uvic.ca>
2024-06-18 13:06:19 -0700

    created tests for getting listing info

--- backend/listing/controller/get_listing.js
+++ backend/listing/controller/get_listing.js
@@ -1,14 +1,8 @@
-import axiosInstance from "../config/axios.js";
+import axios from 'axios';
 
 export const getListingInfo = async (req, res) => {     
     try{
-        // retrieve listing id from request url
-        const listingId = req.params.listingId;
-        const location = { type: 'Point', coordinates: [1,-1]};
-        //retrieve listing information from database
-        const response = await axiosInstance.get(`/listing/${listingId}`);
-        
-        console.log(response);
+        const response = await axios.get(`/listing/${req.params.listingId}`);
         res.json(response.data);
     }
     catch(err){
