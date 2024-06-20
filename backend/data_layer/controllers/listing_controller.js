e5aaccc16e26158f73f13fd0cd7bedde1f3a3e65
Harsh Patel <48337893+harshpatel010202@users.noreply.github.com>
2024-06-20 16:22:45 -0700

    fix: fixed the structure

--- backend/data_layer/controllers/listing_controller.js
+++ backend/data_layer/controllers/listing_controller.js
@@ -21,28 +21,6 @@ export const createListing = (req, res) => {
     });
 };
 
-// export const updateListing = (req, res) => {
-//     Listing.update({
-//         title: req.body.title,
-//         price: req.body.price,
-//         location: req.body.location,
-//         status: "AVAILABLE",
-//         category: req.body.category
-//     },
-//     { where: { _id: req.params.listing_id } 
-//     })
-//     .then((result) => {
-//         return res.json({
-//             message: "Updated Listing"
-//         });
-//     })
-//     .catch((error) => {
-//         return res.json({
-//             message: "Unable to update listing"
-//         });
-//     });
-// };
-
 export const updateListing = async (req, res) => {
     try {
         const listing = await Listing.findByPk(req.params.listing_id);
