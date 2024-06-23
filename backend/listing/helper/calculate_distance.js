24054837ead72952fa4ff7f06ac38215c938d46a
Harsh Patel <48337893+harshpatel010202@users.noreply.github.com>
2024-06-23 16:20:02 -0700

    feat: calculate distance between a user and list of listing

---
+++ backend/listing/helper/calculate_distance.js
@@ -0,0 +1,37 @@
+import geolib from 'geolib';
+
+const user_id = 1;
+const user_location = { latitude: 51.525, longitude: 7.4575 };
+
+const calculateDistance = (user_id, listing_id, user_location, listing_location) => {
+    const distance = geolib.getDistance(user_location, listing_location);
+    if (distance <= 5000) {
+        return listing_id;
+    }
+    return null;
+    };
+
+const listings = [
+    {
+        listing_id: 1,
+        listing_location: { latitude: 51.525, longitude: 8.4575 }
+    },
+    {
+        listing_id: 2,
+        listing_location: { latitude: 51.525, longitude: 8.4575 }
+    },
+    {
+        listing_id: 3,
+        listing_location: { latitude: 52.525, longitude: 7.4575 }
+    },
+    {
+        listing_id: 4,
+        listing_location: { latitude: 51.525, longitude: 7.4575 }
+    }
+];
+
+const result = listings.map(listing => calculateDistance(user_id, listing.listing_id, user_location, listing.listing_location)).filter(listing => listing !== null);
+
+
+// console.log(result);
+
