b34323885159a2497fe2fcd8c6dc2c2436e5726b
Amanda Anderson <aganderson@shaw.ca>
2024-06-24 09:02:02 -0700

    Use Sample Data for testing ManageListings

--- frontend/src/__tests__/pages/ManageListings.test.js
+++ frontend/src/__tests__/pages/ManageListings.test.js
@@ -3,13 +3,7 @@ import { render, screen, } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { BrowserRouter as Router } from 'react-router-dom';
 import ManageListings from '../../pages/ManageListings';
-
-const listings = [
-    { id: '4', title: 'Test 1', price: '2.00', location: 'V9V 9W9', category: 'Furniture', status: 'AVAILABLE' },
-    { id: '10',title: 'Super cool object', price: '3.45', location: 'V9V 9W9', category: 'Technology',status: 'SOLD' },
-    { id: '100',title: 'Buy Me!', price: '1234.56', location: 'V9V 9W9', category: 'SchoolSupplies',status: 'AVAILABLE' },
-    { id: '3',title: 'Another listings for sale', price: '98765432.10', location: 'V9V 9W9', category: 'Furniture', status: 'AVAILABLE' }
-  ];
+import { SAMPLE_LISTING_LIST } from '../TestData';
 
 describe('ManageListings page', () => {
   beforeEach(() => {
