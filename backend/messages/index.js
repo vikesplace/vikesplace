ae04a3678ab1f7a6267b64a3974711371da05945
Naoya Pimlott <naoyapimlott@Naoyas-MacBook-Air.local>
2024-06-24 22:12:11 -0700

    added basic authorization to each service

--- backend/messages/index.js
+++ backend/messages/index.js
@@ -1,5 +1,8 @@
 import express from "express";
 import messages from "./routes/messages.js";
+import jwt from "jsonwebtoken";
+import cookieParser from "cookie-parser";
+import "dotenv/config";
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -8,13 +11,20 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
+app.use(cookieParser());
+app.use(express.json());
 app.use(identification);
 app.use("/messages", messages);
 
-
 function identification(req, res, next) {
-    console.log("Auth middleware logic here");
+  try {
+    const jwtSecret = process.env.ACCESS_TOKEN_SECRET;
+    const decoded = jwt.verify(req.cookies.Authorization, jwtSecret);
+    res.locals.decodedToken = decoded;
     next();
+  } catch (err) {
+    res.json({ message: err.message });
+  }
 }
 
 app.listen(PORT, () => {
