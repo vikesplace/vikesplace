1a2a7f625605c0dd952052da4ee721029704b130
Harsh Patel <48337893+harshpatel010202@users.noreply.github.com>
2024-06-24 19:02:20 -0700

    fix: removed user_id

--- backend/listing/helper/calculate_distance.js
+++ backend/listing/helper/calculate_distance.js
@@ -1,9 +1,8 @@
 import geolib from 'geolib';
 
-const user_id = 1;
 const user_location = { latitude: 51.525, longitude: 7.4575 };
 
-const calculateDistance = (user_id, listing_id, user_location, listing_location) => {
+const calculateDistance = (listing_id, user_location, listing_location) => {
     const distance = geolib.getDistance(user_location, listing_location);
     if (distance <= 5000) {
         return listing_id;
@@ -30,8 +29,4 @@ const listings = [
     }
 ];
 
-const result = listings.map(listing => calculateDistance(user_id, listing.listing_id, user_location, listing.listing_location)).filter(listing => listing !== null);
-
-
-// console.log(result);
-
+const result = listings.map(listing => calculateDistance(listing.listing_id, user_location, listing.listing_location)).filter(listing => listing !== null);
