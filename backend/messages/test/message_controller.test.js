e18a68b811e44367cd5ab015edf9c53246c725d9
Harsh Patel <48337893+harshpatel010202@users.noreply.github.com>
2024-06-25 11:47:56 -0700

    feat: added test cases for get all messages

--- backend/messages/test/message_controller.test.js
+++ backend/messages/test/message_controller.test.js
@@ -4,4 +4,63 @@ import { getAllMessages } from "../controller/get_all_messages";
 jest.mock('axios');
 
 describe("Message Routes", () => {
+    it("it should return all messages", async () => {
+        const mockOutput = [
+            {
+                message_id: 1,
+                message_content: "Hello World",
+                listing_id: 1,
+                timestamp: "2024-06-17T06:25:41.995Z"
+            },
+            {
+                message_id: 2,
+                message_content: "Hello World Again",
+                listing_id: 1,
+                timestamp: "2024-06-18T06:25:41.995Z"
+            }
+        ];
+
+        axios.get.mockImplementation(() =>
+            Promise.resolve({
+                data: mockOutput
+            })
+        );
+
+        let responseObject = {};
+        const mockRes = {
+            body: {},
+            json: jest.fn().mockImplementation((result) => {
+                responseObject = result;
+            }),
+            status: jest.fn(),
+        };
+        await getAllMessages(
+            {
+                params: {
+                    chatId: "1",
+                },
+            }, mockRes);
+        expect(responseObject).toEqual(mockOutput);
+    });
+
+    it("it should fail to return all messages", async () => {
+        axios.get.mockImplementation(() =>
+            Promise.resolve({ data: { message: "Chat id not found" }})
+        );
+        let responseObject = {};
+        const mockRes = {
+            body: {},
+            json: jest.fn().mockImplementation((result) => {
+                responseObject = result;
+            }),
+            status: jest.fn(),
+        };
+        await getAllMessages(
+            {
+                params: {
+                    chatId: "1",
+                },
+            }, mockRes);
+        expect(responseObject).toEqual({ message: "Chat id not found"});
+    });
 });
