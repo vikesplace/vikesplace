3089824220252b30ae409e872845634ae70cabbb
Naoya Pimlott <46671961+ntpimlott@users.noreply.github.com>
2024-06-25 11:00:29 -0700

    fixed merged conflicts that were merged inton main (#80)
    Co-authored-by: Naoya Pimlott <naoyapimlott@Naoyas-MacBook-Air.local>

--- backend/listing/routes/listing.js
+++ backend/listing/routes/listing.js
@@ -3,11 +3,7 @@ import {createListing} from "../controller/create_listing.js";
 import {deleteListing} from "../controller/delete_listing.js";
 import {updateListing} from "../controller/update_listing.js";
 import { getSellerListings } from "../controller/get_seller_listings.js";
-<<<<<<< be-id-223
-=======
 import { getSortedListings } from "../controller/get_sorted_listings.js";
-
->>>>>>> main
 import {getListingInfo} from "../controller/get_listing.js";
 const router = express.Router();
 
