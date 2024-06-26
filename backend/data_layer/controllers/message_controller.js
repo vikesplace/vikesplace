f8fc697051cfdc83f45dfc526831ee70f98a644f
Rena Kollmann-Suhr <renakollmannsuhr@uvic.ca>
2024-06-25 18:43:00 -0700

    created controller and wrote tests

--- backend/data_layer/controllers/message_controller.js
+++ backend/data_layer/controllers/message_controller.js
@@ -1,4 +1,6 @@
 import Messages from "../models/message_models.js";
+import Chats from "../models/chat_models.js";
+import Listings from "../models/listing_models.js";
 
 export const getAllMessages = async (req, res) => {
     try {
@@ -22,6 +24,30 @@ export const getAllMessages = async (req, res) => {
     } catch (error) {
         res.json({
             message: "Chat id not found"
+        });
+    }
+};
+
+export const createChat = async (req, res) => {
+    try {
+        //get user_2 from listings table
+        const listing_entry = await Listings.findOne({
+            where: {
+                listing_id: req.params.listingId
+            }
+        });
+        const chat_entry = await Chats.create(
+            {
+                listing_id: req.params.listingId,
+                user_id_one: res.body.userId,
+                user_id_two: listing_entry.user_id,
+            }
+        );
+
+        res.json(chat_entry);
+    } catch (error) {
+        res.json({
+            message: "Could not create chat"
         });
     }
 };
