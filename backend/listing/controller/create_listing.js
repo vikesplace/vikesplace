29604c64464dc780521a8c6f1df5bc2e9a750d74
Naoya Pimlott <naoyapimlott@Naoyas-MacBook-Air.local>
2024-06-25 23:12:31 -0700

    create listing fix

--- backend/listing/controller/create_listing.js
+++ backend/listing/controller/create_listing.js
@@ -2,14 +2,15 @@ import axios from 'axios';
 
 export const createListing = async (req, res) => {
     try{
-        //Temporary Location until Conversion Setup
-        const location = { type: 'Point', coordinates: [1,-1]};
         const seller_id = res.locals.decodedToken.userId;
+        const geoPoint = await axios(`/listing/location/${req.body.location}`);
+        const geoPointLocation = geoPoint.data;
         const response = await axios.post("/listing",{
                     title: req.body.title,
                     seller_id: seller_id,
                     price: req.body.price,
-                    location: location,
+                    location: geoPointLocation,
+                    postal_code: req.body.location,
                     category: req.body.category
                 });
                 res.json(response.data);
