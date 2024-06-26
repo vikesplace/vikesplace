28211235505589b9c1cd9d93f96879b2e0733391
palsrivastava <104030384+palsrivastava@users.noreply.github.com>
2024-06-25 17:56:35 -0700

    added categories to listing + updated sorting/filters (#71)
    * added categories to listing + updated sorting/filters
    
    * added categories to edit/manage listing

--- frontend/src/components/ListingCard.js
+++ frontend/src/components/ListingCard.js
@@ -6,11 +6,11 @@ import Typography from '@mui/material/Typography';
 import { CardActionArea, CardHeader } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-export default function ListingCard({ id, title, price, location, status }) {
+export default function ListingCard({ id, title, price, location, status, category}) {
   return (
     <Card>
       <CardActionArea component={Link} to={`/listings/${id}`}>
-        <CardHeader title={title} />
+        <CardHeader title={title} subheader={category}/>
         <CardContent>
           <Box sx={{ display: 'flex', alignItems: 'center' }}>
             <Box sx={{ display: 'flex', width: 1 / 3, flexDirection: 'column', mx: 2, alignItems: 'start' }}>
--- frontend/src/pages/ManageListings.js
+++ frontend/src/pages/ManageListings.js
@@ -9,8 +9,8 @@ import '../App.css';
 // TODO GET with api calls
 const listings = [
   { id: '4', title: 'Test 1', price: '2.00', location: 'V9V 9W9', category: 'Furniture', status: 'AVAILABLE' },
-  { id: '10',title: 'Super cool object', price: '3.45', location: 'V9V 9W9', category: 'Technology',status: 'SOLD' },
-  { id: '100',title: 'Buy Me!', price: '1234.56', location: 'V9V 9W9', category: 'SchoolSupplies',status: 'AVAILABLE' },
+  { id: '10',title: 'Super cool object', price: '3.45', location: 'V9V 9W9', category: 'Electronics',status: 'SOLD' },
+  { id: '100',title: 'Buy Me!', price: '1234.56', location: 'V9V 9W9', category: 'Office Supplies',status: 'AVAILABLE' },
   { id: '3',title: 'Another listings for sale', price: '98765432.10', location: 'V9V 9W9', category: 'Furniture', status: 'AVAILABLE' }
 ];
 
@@ -37,7 +37,8 @@ function ManageListings() {
                 title={listing.title}
                 price={listing.price}
                 location={listing.location}
-                status={listing.status}          
+                status={listing.status}
+                category={listing.category}          
               />
               <br />
             </div>
