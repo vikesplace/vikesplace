760d6684438d3416bf06381df84b73d9677d8d38
Naoya Pimlott <naoyapimlott@Naoyas-MacBook-Air.local>
2024-06-29 15:03:18 -0700

    adjusted the calculate distance and added it to sort list

--- backend/listing/helper/calculate_distance.js
+++ backend/listing/helper/calculate_distance.js
@@ -1,32 +1,12 @@
-import geolib from 'geolib';
+import geolib from "geolib";
 
-const user_location = { latitude: 51.525, longitude: 7.4575 };
+export const calculateDistance = (user_location, listing_location) => {
+  const distance = geolib.getDistance(user_location, listing_location);
+  if (distance <= 5000) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-const calculateDistance = (listing_id, user_location, listing_location) => {
-    const distance = geolib.getDistance(user_location, listing_location);
-    if (distance <= 5000) {
-        return listing_id;
-    }
-    return null;
-    };
-
-const listings = [
-    {
-        listing_id: 1,
-        listing_location: { latitude: 51.525, longitude: 8.4575 }
-    },
-    {
-        listing_id: 2,
-        listing_location: { latitude: 51.525, longitude: 8.4575 }
-    },
-    {
-        listing_id: 3,
-        listing_location: { latitude: 52.525, longitude: 7.4575 }
-    },
-    {
-        listing_id: 4,
-        listing_location: { latitude: 51.525, longitude: 7.4575 }
-    }
-];
-
-const result = listings.map(listing => calculateDistance(listing.listing_id, user_location, listing.listing_location)).filter(listing => listing !== null);
+// const result = listings.map(listing => calculateDistance(listing.listing_id, user_location, listing.listing_location)).filter(listing => listing !== null);
