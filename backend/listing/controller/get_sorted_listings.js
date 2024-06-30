be56c352e0d1ca173196af95f4f97a0c9ea8b5d6
Naoya Pimlott <46671961+ntpimlott@users.noreply.github.com>
2024-06-29 21:28:57 -0700

    adjusted the calculate distance and added it to sort list (#103)
    * adjusted the calculate distance and added it to sort list
    
    * removed commented code
    
    ---------
    
    Co-authored-by: Naoya Pimlott <naoyapimlott@Naoyas-MacBook-Air.local>

--- backend/listing/controller/get_sorted_listings.js
+++ backend/listing/controller/get_sorted_listings.js
@@ -1,7 +1,12 @@
 import axios from "axios";
+import { calculateDistance } from "../helper/calculate_distance.js";
 
 export const getSortedListings = async (req, res) => {
   try {
+    const userId = res.locals.decodedToken.userId;
+    const user = await axios.get(`/user/${userId}`);
+    const userCoordinates = user.data.user.location.coordinates;
+
     const response = await axios.get(`/listing`, {
       params: {
         pullLimit: req.query.pullLimit,
@@ -10,16 +15,31 @@ export const getSortedListings = async (req, res) => {
         maxPrice: req.query.maxPrice,
         status: req.query.status,
         sortBy: req.query.sortBy,
-        isDescending: req.query.isDescending
+        isDescending: req.query.isDescending,
       },
     });
-    res.json(response.data);
+
+    if (response.data.rows) {
+      const filteredKm = response.data.rows.filter((listing) => {
+        if (
+          calculateDistance(userCoordinates, listing.location.coordinates) ===
+          true
+        ) {
+          return true;
+        }
+      });
+      res.json(filteredKm);
+    } else {
+      res.json(response.data.rows);
+    }
   } catch (err) {
-    if (err.response && (err.response.status == 400)) { // if bad request, return error to client
+    if (err.response && err.response.status == 400) {
+      // if bad request, return error to client
       return res.status(400).json({ message: err.response.data.message });
-    } else { // if internal server error, log error and return message to client
-        console.error(err);
-        return res.status(500).json({ message: "Failed to get listings"});
+    } else {
+      // if internal server error, log error and return message to client
+      console.error(err);
+      return res.status(500).json({ message: "Failed to get listings" });
     }
   }
 };
