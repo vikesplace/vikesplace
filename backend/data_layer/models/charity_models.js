b1d493b05a5b56bd9a2e68d89646d27f4070ea6c
Harsh Patel <48337893+harshpatel010202@users.noreply.github.com>
2024-07-10 03:46:29 -0700

    created charity table

---
+++ backend/data_layer/models/charity_models.js
@@ -0,0 +1,55 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/database.js";
+
+class Charity extends Model {}
+
+Charity.init(
+    {
+        charity_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        status: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isIn: [["AVAILABLE", "CLOSED"]],
+            },
+        },
+        fund: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: false,
+        },
+        logo_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        start_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+            defaultValue: DataTypes.NOW,
+        },
+        end_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        num_listings: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        timestamps: false,
+    }
+);
+
+export default Charity;
+
+    
