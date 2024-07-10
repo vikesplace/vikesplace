615cde2f139680daf38e026c7d7c367eaa309d63
Amanda Anderson <aganderson@shaw.ca>
2024-07-09 18:06:50 -0700

    MessageHistory

--- frontend/src/__tests__/pages/MessageHistory.test.js
+++ frontend/src/__tests__/pages/MessageHistory.test.js
@@ -3,6 +3,7 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import MessageHistory from '../../pages/MessageHistory';
+import { SAMPLE_CHATS } from '../../utils/SampleRecommenderData';
 
 const renderWithRouter = (ui, { route = '/' } = {}) => {
   window.history.pushState({}, 'Test page', route);
@@ -15,10 +16,6 @@ const renderWithRouter = (ui, { route = '/' } = {}) => {
   );
 };
 
-beforeAll(() => {
-  window.alert = jest.fn();
-});
-
 describe('MessageHistory Component', () => {
   test('renders without crashing', () => {
     renderWithRouter(<MessageHistory />, { route: '/message-history/3' });
@@ -27,7 +24,10 @@ describe('MessageHistory Component', () => {
   test('displays the correct chat information', () => {
     renderWithRouter(<MessageHistory />, { route: '/message-history/3' });
 
-    expect(screen.getByText(/Message .* about .*/)).toBeInTheDocument();
+    expect(screen.getByText(/Message/)).toBeInTheDocument();
+    expect(screen.getByText(/Person's Name/)).toBeInTheDocument();
+    expect(screen.getByText(/about/)).toBeInTheDocument();
+    expect(screen.getByText(/Title of this item I would like to buy/)).toBeInTheDocument();
     expect(screen.getByText(/I'm interested in buying this item.../)).toBeInTheDocument();
     expect(screen.getByText(/Could I ask for a \$5 discount\?/)).toBeInTheDocument();
     expect(screen.getByText(/How long have you had this item\?/)).toBeInTheDocument();
@@ -38,6 +38,7 @@ describe('MessageHistory Component', () => {
   });
 
   test('sends a new message when clicking the send button', () => {
+    jest.spyOn(window, 'alert').mockImplementation(() => {});
     renderWithRouter(<MessageHistory />, { route: '/message-history/3' });
 
     const inputElement = screen.getByPlaceholderText('Type here...');
