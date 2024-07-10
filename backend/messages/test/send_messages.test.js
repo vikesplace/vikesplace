e04c069b29d3877908c1a1a2e8aecfb6b79e511e
Harsh Patel <48337893+harshpatel010202@users.noreply.github.com>
2024-07-10 02:51:34 -0700

    fixed test cases for messages

--- backend/messages/test/send_messages.test.js
+++ backend/messages/test/send_messages.test.js
@@ -6,35 +6,22 @@ jest.mock("axios");
 describe("Send Messages Test", () => {
   it("should send a message", async () => {
     const mockOutput = {
-      message: "Invalid input data",
+      message_id: 1,
+      listing_id: 1,
+      chat_id: 1,
+      message_content: "Hello",
+      sender_id: 1,
+      receiver_id: 2
     };
-    axios.post.mockImplementation(() => Promise.resolve({ data: {} }));
-    let responseObject = {};
-    const mockRes = {
-      body: {},
-      json: jest.fn().mockImplementation((result) => {
-        responseObject = result;
-      }),
-      status: jest.fn(),
-      locals: { decodedToken: { userId: 1 } },
-    };
-    await sendMessages(
-      {
-        params: {
-          chatId: "1",
-        },
-      },
-      mockRes
-    );
-    expect(responseObject).toEqual(mockOutput);
-  });
-  it("should fail to send a message", async () => {
     axios.post.mockImplementation(() =>
-      Promise.resolve({ data: { message: "Could not send message" } })
+      Promise.resolve({
+        data: mockOutput,
+      })
     );
+
+
     let responseObject = {};
     const mockRes = {
-      body: {},
       json: jest.fn().mockImplementation((result) => {
         responseObject = result;
       }),
@@ -46,7 +33,31 @@ describe("Send Messages Test", () => {
         body: {
           content: "Hello",
         },
-        params: { chatId: "123" },
+        params: { chatId: "1" },
+      },
+      mockRes
+    );
+    expect(responseObject).toEqual(mockOutput);
+  });
+
+  it("should fail to send a message", async () => {
+    axios.post.mockImplementation(() =>
+      Promise.resolve({ data: { message: "Could not send message" } })
+    );
+    let responseObject = {};
+    const mockRes = {
+      json: jest.fn().mockImplementation((result) => {
+        responseObject = result;
+      }),
+      status: jest.fn(),
+      locals: { decodedToken: { userId: 1 } },
+    };
+    await sendMessages(
+      {
+        body: {
+          content: "Hello",
+        },
+        params: { chatId: "1" },
       },
       mockRes
     );
