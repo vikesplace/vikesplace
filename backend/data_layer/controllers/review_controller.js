bcc57008963fdc1ca092c855b13d5197e5473528
Harsh Patel <48337893+harshpatel010202@users.noreply.github.com>
2024-07-11 12:28:11 -0700

    be-s3-154: create a review (#113)
    * feat: create a review
    
    * feat: added tests
    
    * fix create review
    
    * fixed tests
    
    * fixed endpoint response to meet the API agreement
    
    * resolve conflicts

--- backend/data_layer/controllers/review_controller.js
+++ backend/data_layer/controllers/review_controller.js
@@ -1,24 +1,25 @@
 import Reviews from "../models/review_models.js";
 
-export const createReview = (req, res) => {
-    Review.create({
-        listing_id: req.body.listing_id,
-        user_id: req.body.user_id,
-        review_content: req.body.review_content,
-        rating_id: req.body.rating_id
-    })
-    .then((result) => {
-        return res.json({
-            message: "Created Review"
-        });
-    })
-    .catch((error) => {
-        console.log(error);
-        return res.json({
-            message: "Unable to create review"
-        });
-    });
-}
+export const createReview = async (req, res) => {
+    try {
+        const reviewResult = await Review.create({
+            listing_id: req.params.listingId,
+            user_id: req.body.user_id,
+            review_content: req.body.review_content,
+            rating_id: req.body.rating_id
+        })
+
+        res.json(reviewResult.dataValues);
+    } catch (error) {
+        if (error.name === 'SequelizeValidationError') {
+            console.error(error);
+            res.status(400).json({ message: error.message });
+        } else {
+            console.error(error);
+            res.status(500).send();
+        }
+    }
+};
 
 export const getAllReviews = async (req, res) => {
     try {
--- backend/ratings_reviews/routes/review.js
+++ backend/ratings_reviews/routes/review.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { createReview } from "../controller/create_review.js";
 import { getAllReviews } from "../controller/get_all_reviews.js";
 
 const router = express.Router();
@@ -6,11 +7,7 @@ const router = express.Router();
 //Get All Reviews for a Listing
 router.get("/:listingId", getAllReviews);
 
-// //Leave a Review for a Listing
-// router.post("/:listingId", (req, res) => {
-//   // Handle login logic
-//   // ...
-//   res.json({ message: "Leave Review" });
-// });
+//Leave a Review for a Listing
+router.post("/:listingId", createReview);
 
 export default router;
