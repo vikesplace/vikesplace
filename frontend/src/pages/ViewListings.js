bfc6603530f97bd0831883dfd65e9de576c7cd4f
Amanda Anderson <75967325+AmandaGAnderson@users.noreply.github.com>
2024-07-16 16:14:12 -0700

    fe: Integrate Listing Endpoints (#144)
    * Get create listing endpoint working
    
    * Remove static data from create listing
    
    * Stop using an object to pass id
    
    * Remove category from manage listing
    
    * Return axios response from data service and make create listing async so that promise has resolved
    
    * Get seller listings on manage listings
    
    * getListing working on edit listing page
    
    * Add forCharity to edit listing, get delete listing endpoint working, setup edit listing endpoint but not yet working
    
    * Fix useEffect warning on view lisitngs
    
    * Remove console log message from create listing to be consistent
    
    * Listing details use api for get listing
    
    * async getMyUserData and comment out getSortedListings so that ViewListings page loads
    
    * Leave comments for send message from listing details component
    
    * remove another console log
    
    * Make most other data service methods async (except those in use)
    
    * Use consistent messaging by modifying connection error message
    
    * Update user calls in dataservice to use me, update getSortedListing to use query params
    
    * Protect edit listing to only user who is selling listing
    
    * Able to view listings with getSortedListings
    
    * Remove category from listing card
    
    * Undo changes to edit listing
    
    * Improve getSortedListing query construction
    
    * Remove category from filters
    
    * Remove unused
    
    * Get sorting working for view listings
    
    * Update sorting direction with an icon button

--- frontend/src/pages/ViewListings.js
+++ frontend/src/pages/ViewListings.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
@@ -20,25 +20,19 @@ import ListingCard from '../components/ListingCard';
 import SearchBar from '../components/SearchBar';
 import { Typography } from '@mui/material';
 import DataService from '../services/DataService';
-import { SAMPLE_DATA } from '../utils/SampleRecommenderData';
-
-const categories = [
-  'Electronics', 'Phones', 'Vehicles', 'Entertainment', 'Garden', 'Outdoor', 'Sports', 'Kitchen Supplies', 'Furniture', 'Musical Instruments', 'Office Supplies', 'Apparel', 'Books', 'Beauty', 'Health'
-];
+import SwapVertIcon from '@mui/icons-material/SwapVert';
+import IconButton from '@mui/material/IconButton';
 
 function ViewListings() {
-  const dataService = new DataService();
+  const dataService = useMemo(() => new DataService(), []);
   const navigate = useNavigate();
 
-  // TODO remove once api is working
-  let initialListings = SAMPLE_DATA;
-
-  const [sortCategory, setSortCategory] = useState('');
+  const [sortCategory, setSortCategory] = useState("listed_at");
+  const [sortOrder, setSortOrder] = useState(false);
   const [priceRange, setPriceRange] = useState({ min: '', max: '' });
   const [statusFilter, setStatusFilter] = useState('');
-  const [categoryFilter, setCategoryFilter] = useState('');
   const [location, setLocation] = useState('Fetching...');
-  const [listings, setListings] = useState(initialListings);
+  const [listings, setListings] = useState([]);
   const [openFilterDialog, setOpenFilterDialog] = useState(false);
   const [openLocationDialog, setOpenLocationDialog] = useState(false);
   const [newLocation, setNewLocation] = useState('');
@@ -48,48 +42,55 @@ function ViewListings() {
 
   useEffect(() => {
     const fetchListings = async () => {
-      const response = await dataService.getSortedListings(priceRange.min, priceRange.max, statusFilter, sortCategory, false); 
-      if (response !== undefined) {
+      const response = await dataService.getSortedListings(priceRange.min, priceRange.max, statusFilter, sortCategory, sortOrder); 
+      if (response === undefined) {
+        alert("Connection error, please try again.");
+      } else if (response.status === 200) {
         setListings(response.data);
+      } else {
+        alert("Unable to get listings, please try again.");
       }
     };
 
     const fetchLocation = async () => {
       const response = await dataService.getMyUserData();
-      setLocation(response !== undefined ? response.data : "V8V2G4");
+      setLocation(response !== undefined ? response.data.location : "V8V2G4");
     };
 
     fetchListings();
     fetchLocation();
-  }, [priceRange, statusFilter, sortCategory]);
+  }, [dataService, priceRange, statusFilter, sortCategory, sortOrder]);
 
   const handleListingClick = (id) => {
     navigate(`/listings/${id}`);
   };
 
-  const handleSortChange = (event) => {
+  const handleSortChange = async (event) => {
     const category = event.target.value;
     setSortCategory(category);
 
-    let sortedListings = [...listings];
-    switch (category) {
-      case 'price':
-        sortedListings.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
-        break;
-      case 'location':
-        sortedListings.sort((a, b) => a.location.localeCompare(b.location));
-        break;
-      case 'status':
-        sortedListings.sort((a, b) => a.status.localeCompare(b.status));
-        break;
-      case 'category':
-        sortedListings.sort((a, b) => a.category.localeCompare(b.category));
-        break;
-      default:
-        sortedListings = initialListings;
-        break;
+    const response = await dataService.getSortedListings(priceRange.min, priceRange.max, statusFilter, sortCategory, sortOrder); 
+    if (response === undefined) {
+      alert("Connection error, please try again.");
+    } else if (response.status === 200) {
+      setListings(response.data);
+    } else {
+      alert("Unable to get listings, please try again.");
     }
-    setListings(sortedListings);
+  };
+
+  const handleSortOrderClick = async () => {
+    const currOrder = sortOrder;
+    setSortOrder(!currOrder);
+
+    const response = await dataService.getSortedListings(priceRange.min, priceRange.max, statusFilter, sortCategory, sortOrder); 
+    if (response === undefined) {
+      alert("Connection error, please try again.");
+    } else if (response.status === 200) {
+      setListings(response.data);
+    } else {
+      alert("Unable to get listings, please try again.");
+    }
   };
 
   const handlePriceRangeChange = (event) => {
@@ -101,24 +102,15 @@ function ViewListings() {
     setStatusFilter(event.target.value);
   };
 
-  const handleCategoryFilterChange = (event) => {
-    setCategoryFilter(event.target.value);
-  };
-
   const applyFilters = async () => {
-    const filteredListings = initialListings.filter(listing => {
-      const inPriceRange = (priceRange.min === '' || parseFloat(listing.price) >= parseFloat(priceRange.min)) &&
-                          (priceRange.max === '' || parseFloat(listing.price) <= parseFloat(priceRange.max));
-      const matchesStatus = statusFilter === '' || listing.status === statusFilter;
-      const matchesCategory = categoryFilter === '' || listing.category === categoryFilter;
-      return inPriceRange && matchesStatus && matchesCategory;
-    });
-    setListings(filteredListings);
-
-    const response = await dataService.getSortedListings(priceRange.min, priceRange.max, statusFilter, sortCategory, false); 
-    if (response !== undefined) {
+    const response = await dataService.getSortedListings(priceRange.min, priceRange.max, statusFilter, sortCategory, sortOrder); 
+    if (response === undefined) {
+      alert("Connection error, please try again.");
+    } else if (response.status === 200) {
       setListings(response.data);
-    } 
+    } else {
+      alert("Unable to get listings, please try again.");
+    }
     setOpenFilterDialog(false);
   };
 
@@ -171,22 +163,25 @@ function ViewListings() {
       <Container>
         <SearchBar />
         <Box mt={2} display="flex" justifyContent="space-between" alignItems="center">
-          <FormControl sx={{ minWidth: 120, mr: 2 }}>
-            <InputLabel id="sort-select-label">Sort By</InputLabel>
-            <Select
-              labelId="sort-select-label"
-              id="sort-select"
-              value={sortCategory}
-              label="Sort By"
-              onChange={handleSortChange}
-            >
-              <MenuItem value=""><em>None</em></MenuItem>
-              <MenuItem value="price">Price</MenuItem>
-              <MenuItem value="location">Location</MenuItem>
-              <MenuItem value="status">Status</MenuItem>
-              <MenuItem value="category">Category</MenuItem>
-            </Select>
-          </FormControl>
+          <Box id="sort-options" display="flex"> 
+            <FormControl sx={{ minWidth: 120, mr: 2 }}>
+              <InputLabel id="sort-select-label">Sort By</InputLabel>
+              <Select
+                labelId="sort-select-label"
+                id="sort-select"
+                value={sortCategory}
+                label="Sort By"
+                onChange={handleSortChange}
+              >
+                <MenuItem value="listed_at"><em>Time</em></MenuItem>
+                <MenuItem value="price">Price</MenuItem>
+                {/* <MenuItem value="location">Distance</MenuItem> */}
+              </Select>
+            </FormControl>
+            <IconButton aria-label="change sorting order" onClick={handleSortOrderClick}>
+              <SwapVertIcon />
+            </IconButton>
+          </Box>
           <Button
             variant="outlined"
             color="primary"
@@ -214,20 +209,19 @@ function ViewListings() {
           sx={{ display: 'flex', justifyContent: 'center', mt: 2 }}
         />
         <Box mt={2}>
-          {(listings.length === 0) && 
+          {(listings === undefined || listings.length === 0) && 
             <Typography align="center" variant='h6'>
               No Listings Meet Criteria
             </Typography>
           }
-          {paginatedListings.map((listing) => (
-            <div key={listing.id} onClick={() => handleListingClick(listing.id)}>
+          {listings !== undefined && paginatedListings.map((listing) => (
+            <div key={listing.listingId} onClick={() => handleListingClick(listing.listingId)}>
               <ListingCard
-                id={listing.id}
+                id={listing.listingId}
                 title={listing.title}
                 price={listing.price}
                 location={listing.location}
                 status={listing.status}
-                category={listing.category}
               />
               <br />
             </div>
@@ -272,21 +266,6 @@ function ViewListings() {
                 <MenuItem value=""><em>None</em></MenuItem>
                 <MenuItem value="AVAILABLE">Available</MenuItem>
                 <MenuItem value="SOLD">Sold</MenuItem>
-              </Select>
-            </FormControl>
-            <FormControl fullWidth sx={{ mt: 2 }}>
-              <InputLabel id="category-filter-label">Category</InputLabel>
-              <Select
-                labelId="category-filter-label"
-                id="category-filter"
-                value={categoryFilter}
-                label="Category"
-                onChange={handleCategoryFilterChange}
-              >
-                <MenuItem value=""><em>None</em></MenuItem>
-                {categories.map((category, index) => (
-                  <MenuItem key={index} value={category}>{category}</MenuItem>
-                ))}
               </Select>
             </FormControl>
           </DialogContent>
