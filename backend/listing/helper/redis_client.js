5e7568639c0ed8a18ed7199f7e46159922dee1a1
smashraf <eshanashraf12@gmail.com>
2024-07-20 13:41:31 -0700

    set up redis caching for listing endpoint

--- backend/listing/helper/redis_client.js
+++ backend/listing/helper/redis_client.js
@@ -2,11 +2,16 @@
 import { createClient } from 'redis';
 
 const client = createClient({
-  url: 'redis://localhost:6379'
+  url: process.env.REDIS_URL || 'redis://localhost:6379'
 });
 
 client.on('error', (err) => console.log('Redis Client Error', err));
 
-await client.connect();
+const connectClient = async () => {
+  await client.connect();
+  console.log('Connected to Redis');
+};
+
+connectClient().catch(console.error);
 
 export default client;
