449cf57ad14b7fbf1ba12855380b36a3dabe9cd5
Naoya Pimlott <naoyapimlott@Naoyas-MacBook-Air.local>
2024-07-25 14:44:13 -0700

    add test and try catch

--- backend/account/routes/logout.js
+++ backend/account/routes/logout.js
@@ -3,8 +3,13 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
+  try {
     res.cookie("Authorization", "", { httpOnly: true });
     res.status(200).json();
+  } catch (err) {
+    console.error(err);
+    res.status(500).json();
+  }
 });
 
 export default router;
