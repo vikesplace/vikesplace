7a5cf9a8de71e003fdb7871c9706c83ba8d06ef5
Amanda Anderson <aganderson@shaw.ca>
2024-07-26 11:39:02 -0700

    Mock useParams and axios on edit listing page

--- frontend/src/__tests__/pages/EditListing.test.js
+++ frontend/src/__tests__/pages/EditListing.test.js
@@ -1,28 +1,74 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { BrowserRouter as Router } from 'react-router-dom';
 import EditListing from '../../pages/EditListing';
+import mockAxios from 'jest-mock-axios';
+
+const API_URL = "http://localhost:8080/";
 
 // TODO get this to mock useParams correctly
-/* jest.mock('react-router-dom', () => ({
+jest.mock('react-router-dom', () => ({
   ...jest.requireActual('react-router-dom'),
   useParams: jest.fn(),
-})); */
+})); 
 
 describe('EditListing page', () => {
-  beforeEach(() => {
+  afterEach(() => {
+    mockAxios.reset();
+    jest.clearAllMocks();
+  });
+
+  test('renders edit listing form with no listing', () => {
+    const id = 'invalid';
+    jest.spyOn(require('react-router-dom'), 'useParams').mockReturnValue({ id: id });
     render(
       <Router>
         <EditListing />
       </Router>
     );
-  });
 
-  test('renders edit listing form', () => {
-    // jest.spyOn(Router, 'useParams').mockReturnValue({ id: 'invalid' });
+    const withCredentials = true;
+    expect(mockAxios.get).toHaveBeenCalledWith(API_URL + 'listings/' + id, 
+      {withCredentials}
+    );
+
+    // simulating a server response
+    let responseObj = { status: 200, data: undefined };
+    mockAxios.mockResponse(responseObj);
+    
     expect(screen.getByText('No Listing Found')).toBeInTheDocument();
   });
 
-  // TODO add test for mocked id/axois call
+  test('renders edit listing with a listing', async () => {
+    const id = '21';
+    jest.spyOn(require('react-router-dom'), 'useParams').mockReturnValue({ id: id });
+    render(
+      <Router>
+        <EditListing />
+      </Router>
+    );
+
+    const withCredentials = true;
+    expect(mockAxios.get).toHaveBeenCalledWith(API_URL + 'listings/' + id, 
+      {withCredentials}
+    );
+
+    // simulating a server response
+    let responseObj = { 
+      status: 200, 
+      data: {
+        listingId: 21,
+        title: "Listing Title",
+        pric: 3.21,
+        location: "V9V9V9",
+        status: "AVAILABLE",
+        forCharity: true,
+      }
+    };
+    mockAxios.mockResponse(responseObj);
+    await waitFor(() => {
+      expect(screen.getByText('Edit Listing')).toBeInTheDocument();
+    })
+  });
 });
