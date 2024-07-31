495833bbed6a26eb506d048ad563681961cd96a6
Amanda Anderson <aganderson@shaw.ca>
2024-07-31 13:12:57 -0700

    Fix one search history test

--- frontend/src/__tests__/pages/SearchHistory.test.js
+++ frontend/src/__tests__/pages/SearchHistory.test.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { render, screen, waitFor, within } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'; 
 import SearchHistory from '../../pages/SearchHistory';
-import { SAMPLE_SEARCHES } from '../../testSetup/TestData';
+import { SAMPLE_LISTING, SAMPLE_SEARCHES } from '../../testSetup/TestData';
 import mockAxios from 'jest-mock-axios';
 
 describe('SeachHistory page', () => {
@@ -23,9 +23,9 @@ describe('SeachHistory page', () => {
     let responseObj = { status: 200, data: undefined };
     mockAxios.mockResponse(responseObj);
     
-    const tableRows = container.querySelectorAll<HTMLElement>('table tbody tr')
-    expect(within(tableRows[0]).getByRole('cell').getByText('No Search History Available')).toBeInTheDocument();
-    // expect(screen.getByText('No Search History Available')).toBeInTheDocument();
+    waitFor(() => {
+      expect(screen.getByText('No Search History Available')).toBeInTheDocument();
+    })
   });
 
   test('renders SearchHistory component with history', async () => {
@@ -40,13 +40,10 @@ describe('SeachHistory page', () => {
     let responseObj = { status: 200, data: {searches: SAMPLE_SEARCHES} };
     mockAxios.mockResponse(responseObj);
 
-    const tableRows = container.querySelectorAll<HTMLElement>('table tbody tr')
-    expect(within(tableRows[0]).queryAllByRole('cell'));
-
-
     await waitFor(() => {
-      const listingCards = screen.getAllByTestId('history-card');
-      expect(listingCards.length).toBe(SAMPLE_SEARCHES.length);
+      SAMPLE_SEARCHES.forEach((search) => {
+        expect(screen.getByText(new RegExp(search.query, "i"))).toBeInTheDocument();
+      });
     })
   });
 });
