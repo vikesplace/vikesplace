b8a18624bed84e9c0957a12d5c5606ea1eeadb88
Amanda Anderson <aganderson@shaw.ca>
2024-07-31 13:02:16 -0700

    Try to add navbar tests for logout and user profile but couldn't check the usenav passed value

--- frontend/src/__tests__/components/NavBar.test.js
+++ frontend/src/__tests__/components/NavBar.test.js
@@ -3,6 +3,16 @@ import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { BrowserRouter as Router } from 'react-router-dom';
 import NavBar from '../../components/NavBar';
+import AuthService from '../../services/AuthService';
+import { Store } from 'react-notifications-component';
+
+// Mock Store for notifications
+jest.mock('react-notifications-component', () => ({
+  Store: {
+    addNotification: jest.fn(),
+  },
+}));
+
 
 let mockUseLocationValue = {
   pathname: "/home",
@@ -20,12 +30,18 @@ jest.mock('react-router-dom', () => ({
   })
 }));
 
+// Mock AuthService
+jest.mock('../../services/AuthService', () => {
+  return jest.fn().mockImplementation(() => {
+    return {
+      logout: jest.fn()
+    };
+  });
+});
+
 describe('NavBar component', () => {
   let useNavigateMock;
-
-  afterEach(() => {
-      jest.clearAllMocks();
-  });
+  let authServiceMock;
 
   beforeEach(() => {  
     render(
@@ -36,6 +52,12 @@ describe('NavBar component', () => {
 
     useNavigateMock = require('react-router-dom').useNavigate;
     jest.spyOn(require('react-router-dom'), 'useNavigate').mockReturnValue(useNavigateMock);
+
+    authServiceMock = new AuthService();
+  });
+
+  afterEach(() => {
+      jest.clearAllMocks();
   });
 
   test('renders component with correct buttons', async () => {
@@ -56,74 +78,100 @@ describe('NavBar component', () => {
   });
 
   test('VikesPlace links to home', () => {
-    useNavigateMock = require('react-router-dom').useNavigate;
-    useNavigateMock.mockReturnValue(jest.fn());
-
     const VikesPlaceText = screen.getByText('VikesPlace');
     fireEvent.click(VikesPlaceText);
 
     expect(useNavigateMock).not.toHaveBeenCalledWith('/');
-
-    jest.clearAllMocks();
   });
 
   test('View Listings links to view-listings', () => {
-    useNavigateMock = require('react-router-dom').useNavigate;
-    useNavigateMock.mockReturnValue(jest.fn());
-
     const linkText = screen.getAllByText('View Listings')[0];
     fireEvent.click(linkText);
 
     expect(useNavigateMock).not.toHaveBeenCalledWith('/view-listings');
-
-    jest.clearAllMocks();
   });
 
   test('Create Listing links to create-listings', () => {
-    useNavigateMock = require('react-router-dom').useNavigate;
-    useNavigateMock.mockReturnValue(jest.fn());
-
     const linkText = screen.getAllByText('Create Listing')[0];
     fireEvent.click(linkText);
 
     expect(useNavigateMock).not.toHaveBeenCalledWith('/create-listings');
-
-    jest.clearAllMocks();
   });
 
   test('Manage Listings links to manage-listings', () => {
-    useNavigateMock = require('react-router-dom').useNavigate;
-    useNavigateMock.mockReturnValue(jest.fn());
-
     const linkText = screen.getAllByText('Manage Listings')[0];
     fireEvent.click(linkText);
 
     expect(useNavigateMock).not.toHaveBeenCalledWith('/manage-listings');
-
-    jest.clearAllMocks();
   });
 
   test('Messages links to messages', () => {
-    useNavigateMock = require('react-router-dom').useNavigate;
-    useNavigateMock.mockReturnValue(jest.fn());
-
     const linkText = screen.getAllByText('Messages')[0];
     fireEvent.click(linkText);
 
     expect(useNavigateMock).not.toHaveBeenCalledWith('/messages');
-
-    jest.clearAllMocks();
   });
 
   test('Charity Events links to charities', () => {
-    useNavigateMock = require('react-router-dom').useNavigate;
-    useNavigateMock.mockReturnValue(jest.fn());
-
     const linkText = screen.getAllByText('Charity Events')[0];
     fireEvent.click(linkText);
 
     expect(useNavigateMock).not.toHaveBeenCalledWith('/view-charities');
+  });
 
-    jest.clearAllMocks();
+  test('logout', async () => {
+    authServiceMock.logout.mockResolvedValueOnce({
+      status: 200,
+    });
+
+    const accountDropdown = screen.getByTestId("account-dropdown");
+    fireEvent.mouseDown(accountDropdown);
+
+    waitFor(() => {
+      expect(screen.getAllByText('Logout')[0]).toBeInTheDocument();
+    })
+
+    fireEvent.mouseDown(screen.getByTestId("logout"));
+
+    expect(useNavigateMock).toHaveBeenCalled();
+  });
+
+  test('logout fails', async () => {
+    authServiceMock.logout.mockResolvedValueOnce({
+      status: 500,
+    });
+
+    const accountDropdown = screen.getByTestId("account-dropdown");
+    fireEvent.mouseDown(accountDropdown);
+
+    waitFor(() => {
+      expect(screen.getAllByText('Logout')[0]).toBeInTheDocument();
+    })
+
+    fireEvent.click(screen.getByTestId("logout"));
+
+    expect(useNavigateMock).not.toHaveBeenCalledWith('/login');
+
+    await waitFor(() => {
+      expect(Store.addNotification).toHaveBeenCalledWith(
+        expect.objectContaining({
+          title: 'Logout Failed',
+          message: 'Please try again',
+        })
+      );
+    });
+  });
+
+  test('view user profile', async () => {
+    const accountDropdown = screen.getByTestId("account-dropdown");
+    fireEvent.mouseDown(accountDropdown);
+
+    waitFor(() => {
+      expect(screen.getAllByText('User Profile')[0]).toBeInTheDocument();
+    })
+
+    fireEvent.click(screen.getByTestId("user-profile"));
+
+    expect(useNavigateMock).toHaveBeenCalled();
   });
 });
